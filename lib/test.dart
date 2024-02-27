01835dac6da24833cc67b0b21b063d821a16af35
RusdyZain <ryanfajri28@gmail.com>
2024-02-27 10:50:56 +0800

    Rechange Setup Gradle

--- lib/test.dart
+++ lib/test.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_application_3/test2.dart';
 
 class Apalah extends StatelessWidget {
